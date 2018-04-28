0366aeef0224b50cfb7ebc9c0f61009fe46747eb
TKman <304000086@qq.com>
2018-04-28 18:42:33 +0800

    注释

--- src/main/java/com/atguigu/userDaoImpl.java
+++ src/main/java/com/atguigu/userDaoImpl.java
@@ -4,12 +4,14 @@ public class userDaoImpl implements userDao {
 
 	@Override
 	public void getUser() {
+		//安妮
 		System.out.println("你看见了我的小熊吗？");
+		//亚索
 		System.out.println("死亡如风常伴吾身！");
+		//狗头
 		System.out.println("我们生他们死！");
+		//薇恩
 		System.out.println("肮脏的蠢货！");
-		
-		
 	}
 
 }
