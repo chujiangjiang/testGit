6f69f56261779e1f3083c8dadf6826d435859f47
TKman <304000086@qq.com>
2018-04-28 18:38:38 +0800

    薇恩更新

--- src/main/java/com/atguigu/userDaoImpl.java
+++ src/main/java/com/atguigu/userDaoImpl.java
@@ -7,6 +7,7 @@ public class userDaoImpl implements userDao {
 		System.out.println("你看见了我的小熊吗？");
 		System.out.println("死亡如风常伴吾身！");
 		System.out.println("我们生他们死！");
+		System.out.println("肮脏的蠢货！");
 		
 		
 	}
